1a053502001a72baeb2ae452c2ac6f5baa19237a
seryl lounis <seryllounis@outlook.fr>
2021-10-28 11:22:55 +0200

    display home photographers"

---
+++ js/home/home.js
@@ -0,0 +1,37 @@
+import ScrollButton from "./ButtonScroll.js";
+import filterTags from "./filterTags.js";
+
+// DISPLAY ALL PHOTOGRAPHERS BY DEFAULT
+export default class HomePageBuilder {
+  // Build the photographers section, call the 'filtertags' function and the 'passer au contenu' button
+  displayPhotographers(data) {
+    let photographers = data.photographers;
+    photographers.map((photographe) => {
+      let sectionPhotographers = document.getElementById("photographers");
+      let articlePhotographers = document.createElement("article");
+      articlePhotographers.className =
+        photographe.tags.join(" ") + " photographer-profil";
+      let templatePhotographer = `
+            <a href="photographers.html?id=${photographe.id}" title="${
+        photographe.name
+      }">
+                <img src="${
+                  photographe.portrait
+                }" alt="${photographe.name} profil picture">
+                <h2 class="name">${photographe.name}</h2>
+            </a>
+            <p class="location">${photographe.city}, ${photographe.country}</p>
+            <p class="tagline">${photographe.tagline}</p>
+            <p class="price">${photographe.price}€/jour</p>
+            <ul class="phtags">${photographe.tags
+              .map((tag) => `<li class="tag" data-filter="${tag}">#${tag}</li>`)
+              .join(" ")}</ul> 
+            `;
+
+      sectionPhotographers.appendChild(articlePhotographers);
+      articlePhotographers.innerHTML = templatePhotographer;
+    });
+    new ScrollButton().displayButton();
+    new filterTags().init();
+  }
+}
